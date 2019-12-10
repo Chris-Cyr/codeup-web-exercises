dd29ee40aa9f26fa89f040872cae31cfb79088b3
Chris Cyr <chris.dana.cyr@gmail.com>
2019-12-10 11:34:17 -0600

    added split join and planets-string exercise

--- js/arrays.js
+++ js/arrays.js
@@ -21,3 +21,34 @@ for (var i = 0; i<favoriteHockeyTeams.length;i++){
 favoriteHockeyTeams.forEach(function(team, i, favoriteHockeyTeams){
     console.log(team);
 })
+
+var fruit = ["apple", "orange", "banana"];
+console.log(fruit);
+fruit.push("pear");
+console.log(fruit);
+
+
+
+var sentence = "The quick brown fox jumps over the lazy dog";
+
+
+
+
+// for (var i = 0;i<words.length; i++) {
+//     // capitalize first letter
+//     var word = words[i];
+//     // var firstLetter = word.split()[0];
+//     var letters = word.split();
+//     var capitalFirstLetter = letters[0].toUpperCase();
+//     var capitalizedWord = "";
+//     capitalizedWord += capitalFirstLetter;
+//     for (var j=1;j<letters.length;j++) {
+//         capitalizedWord += letters[j];
+//     }
+//     words[i] = capitalizedWord;
+// }
+// sentence = words.join(" ");
+// console.log(sentence);
+
+
+
