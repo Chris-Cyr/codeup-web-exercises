24d9189e09ba18873481cd63ed1b235f6c1a08aa
Chris Cyr <chris.dana.cyr@gmail.com>
2019-12-11 14:54:59 -0600

    all in the days work

---
+++ js/objects-lec.js
@@ -0,0 +1,104 @@
+// var car = {
+//     make: "Toyota",
+//     model: "Sienna",
+//     year: 2009,
+//     isTitled: true,
+//     options: ["4 wheel drive",
+//         "heated seats",
+//         "leather interior"],
+//     goGoGo: function(key) {
+//         if (key == "correct") {
+//             console.log("vroom");
+//         }else{
+//             console.log("...");
+//         }
+//     }
+// }
+//
+// console.log("car was made in " + car.year);
+// console.log("car has these options");
+// car.options.forEach(function (option) {
+//     console.log(option)});
+// car.goGoGo("correct");
+
+//
+//
+// var myCar = {
+//     make: "Honda",
+//     model: "Accord",
+//     year: 2017,
+//     isTitled: true,
+//     options: ["Interior lights",
+//         "black wheels",
+//         "7 inch display"],
+//     skirrrt: function(gasPedal) {
+//         if (gasPedal == "mashed") {
+//             console.log("fwaaaaaaaaaaaaaaaaa");
+//         }else{
+//             console.log("push harder fool");
+//         }
+//     }
+// }
+// myCar.options.forEach(function (option) {
+//     console.log(option)});
+// myCar.skirrrt("mashed")
+// // var make = "Toyota";
+// // var model = "Sienna";
+// // var year = 2009;
+// // var isTitled = true;
+// // var goGoGo = function(key){
+// //     if (key == "correct") {
+// //         console.log("vroom");
+// //     }else{
+// //         console.log("...");
+// //     }
+// // }
+//
+// var truck = new Object();
+// var cats = new Array();
+//
+// truck.make = "Tesla";
+// truck.model = "CyberTruck";
+// truck.year = 2022;
+//
+// cats[3];
+// truck["model"];
+//
+//
+// car.report = function(){
+//     console.log(this.year + " " + this.make + " " + this.model)
+//
+//
+// var cars = [
+//     myCar,
+//     {
+//         make:"Dodge",
+//         model: "Challenger",
+//         year: 2015,
+//         report: function(){
+//             console.log(this.year + " " + this.make + " " + this.model)
+//     }},
+//     {
+//         make:"Ford",
+//         model:"Shelby GT 500",
+//         year: 1969,
+//         report: function(){
+//             console.log(this.year + " " + this.make + " " + this.model)
+//     }},
+//     {
+//         make:"Cadillac",
+//         model:"Escalade",
+//         year: 2019,
+//         report: function(){
+//             console.log(this.year + " " + this.make + " " + this.model)
+//         }
+//
+//     }
+// ]
+//
+// cars.forEach(function(car){
+//     car.report();
+// })}
+//
+//
+// // var hockeyTeams =
--- js/planets-string.js
+++ js/planets-string.js
@@ -1,26 +1,26 @@
-(function(){
-    "use strict";
-
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
-
-    /**
-     * TODO:
-     * Convert planetsString to an array, and save it in a variable named
-     * planetsArray.
-     * console.log planetsArray to check your work
-     */
-    var planetsArray = planetsString.split(" ");
-    console.log(planetsArray);
-
-    /**
-     * TODO:
-     * Create a string with <br> tags between each planet. console.log() your
-     * results. Why might this be useful?
-*/
-     var stringPlanets = planetsArray.toString("<br>")
-    console.log(stringPlanets);
-
+// (function() {
+//     "use strict";
+//
+//     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+//     var planetsArray;
+//
+//     /**
+//      * TODO:
+//      * Convert planetsString to an array, and save it in a variable named
+//      * planetsArray.
+//      * console.log planetsArray to check your work
+//      */
+//     var planetsArray = planetsString.split("|");
+//     console.log(planetsArray);
+//
+//     /**
+//      * TODO:
+//      * Create a string with <br> tags between each planet. console.log() your
+//      * results. Why might this be useful?
+//      */
+//     var stringPlanets = planetsArray.join("<br>")
+//     console.log(stringPlanets);
+//
 
     /* BONUS:
     * Create another string that would display your planets in an undordered
@@ -28,4 +28,3 @@
     * string, and <li> tags around each planet.
     */
 
-})();
