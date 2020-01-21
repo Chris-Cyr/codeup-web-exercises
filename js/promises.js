6389b7550365e3428bdeec7e4896d84fe6c27db1
Chris Cyr <chris.dana.cyr@gmail.com>
2020-01-21 15:03:19 -0600

    promises promises

--- js/promises.js
+++ js/promises.js
@@ -12,21 +12,37 @@
 // const request = wait(5000);
 // console.log(request); // pending promise
 
+//
+// function wait2 () {
+//     return fetch('https://api.github.com/repos/Chris-Cyr/codeup-web-exercises/commits/master', {headers: { Authorization: 'token c23b037c1c920d8187477845db810a22dae4db4f'}});
+// }
+//
+// wait2().then((data) => {
+//     console.log(data);
+// })
 
-function wait2 () {
-    return fetch('https://api.github.com/repos/Chris-Cyr/codeup-web-exercises/commits/master', {headers: { Authorization: 'token c23b037c1c920d8187477845db810a22dae4db4f'}});
+
+
+// fetch('https://api.github.com/users')
+//     .then(resp => {
+//         console.log(resp);
+//         return resp.json();
+//     })
+//     .then(payload => {
+//     console.log(payload)
+//     return payload;
+//     })
+//     .then(users => users.map(user => user.login))
+//     .then(usernames => console.log(usernames))
+
+
+const lastCommit = (username) => {
+    let url = `https://api.github.com/users/${username}/events/public`;
+    fetch(url, {headers: {'Authorization': `token ${gitHubKey}`}})
+        .then(resp => resp.json())
+        .then(data => data.find(event => event.type === "PushEvent"))
+    .then(event => event.created_at)
+    .then(date => console.log(date));
+
 }
-
-wait2().then((data) => {
-    console.log(data);
-})
-
-
-
-console.log(fetch('https://api.github.com/repos/Chris-Cyr/codeup-web-exercises/commits/master', {headers: { Authorization: 'token c23b037c1c920d8187477845db810a22dae4db4f'}}));
-
-
-
-
-
-
+lastCommit("chris-cyr")
