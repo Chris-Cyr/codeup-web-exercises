11e0086a0fd73a6af718a00133604e1ceabd714e
chris-cyr <chris.dana.cyr@gmail.com>
2020-06-30 13:10:02 -0500

    Connecting new work station

--- js/project-euler.js
+++ js/project-euler.js
@@ -5,6 +5,7 @@
 //
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
+
 function multiples35(input){
     let sumOfMultiples = 0;
     for(var i = 0; i<input; i++){
@@ -16,3 +17,4 @@ function multiples35(input){
 }
 
 console.log(multiples35(999));
+console.log(multiples35(500));
